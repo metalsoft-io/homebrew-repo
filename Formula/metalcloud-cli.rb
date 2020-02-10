383918c36e3cb035747f40152a175cc130f4af6f
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-10 13:05:47 +0200

    Brew formula update for metalcloud-cli version v1.2.0

--- Formula/metalcloud-cli.rb
+++ Formula/metalcloud-cli.rb
@@ -2,16 +2,16 @@
 class MetalcloudCli < Formula
   desc "Metalcloud's CLI"
   homepage "https://bigstep.com/"
-  version "1.1.20"
+  version "1.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bigstepinc/metalcloud-cli/releases/download/v1.1.20/metalcloud-cli_1.1.20_Darwin_x86_64.tar.gz"
-    sha256 "6677990868a7450ef35a67b43b8787f24f68bfcc0d1c0df9ca6c009ce1202076"
+    url "https://github.com/bigstepinc/metalcloud-cli/releases/download/v1.2.0/metalcloud-cli_1.2.0_Darwin_x86_64.tar.gz"
+    sha256 "944e9b90bf801943a978651589fe1e9a95594f49c1e35369782c901098482bea"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bigstepinc/metalcloud-cli/releases/download/v1.1.20/metalcloud-cli_1.1.20_Linux_x86_64.tar.gz"
-      sha256 "1206b1c1b0658d8fa53c216778a87efa18a2c7df6f1a193a6c31a1e1d0ca5870"
+      url "https://github.com/bigstepinc/metalcloud-cli/releases/download/v1.2.0/metalcloud-cli_1.2.0_Linux_x86_64.tar.gz"
+      sha256 "bb894f49589a85cb046efde2af57d220d51556a911f0f90b26141204f54e7895"
     end
   end
 
