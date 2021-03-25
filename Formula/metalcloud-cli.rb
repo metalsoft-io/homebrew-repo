88463eec610173b8f4e7882386a3bb2d0faf4b9a
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-25 15:38:55 +0200

    Brew formula update for metalcloud-cli version v2.0.7

--- Formula/metalcloud-cli.rb
+++ Formula/metalcloud-cli.rb
@@ -2,16 +2,16 @@
 class MetalcloudCli < Formula
   desc "Metalcloud's CLI"
   homepage "https://bigstep.com/"
-  version "2.0.6"
+  version "2.0.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metalsoft-io/metalcloud-cli/releases/download/v2.0.6/metalcloud-cli_2.0.6_Darwin_x86_64.tar.gz"
-    sha256 "798be62b162510e5b47f632993891090f9b9181ded76ea0ce15db8b1797dec40"
+    url "https://github.com/metalsoft-io/metalcloud-cli/releases/download/v2.0.7/metalcloud-cli_2.0.7_Darwin_x86_64.tar.gz"
+    sha256 "8b8c2245b1f55546240c37e4c7886881ddc6f003f1ba7c9a6588c6a13c21184a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/metalsoft-io/metalcloud-cli/releases/download/v2.0.6/metalcloud-cli_2.0.6_Linux_x86_64.tar.gz"
-    sha256 "fc60156f76be679ea7cc04eeec26ce6c09bf7ecc7b04098bae6fbefcefc429be"
+    url "https://github.com/metalsoft-io/metalcloud-cli/releases/download/v2.0.7/metalcloud-cli_2.0.7_Linux_x86_64.tar.gz"
+    sha256 "43a5a5ee1d9d8b055394361f2ebc51149515bc59bbd7a8d689a714172fbe4473"
   end
 
   def install
